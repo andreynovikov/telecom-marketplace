2248477344b57588866fd33f735141a7e06548cd
Andrey Novikov <novikov@gmail.com>
2024-06-17 17:08:22 +0300

    Stylize subject list

--- frontend/app/(admin)/admin/subjects/page.js
+++ frontend/app/(admin)/admin/subjects/page.js
@@ -1,6 +1,27 @@
+import Card from '@mui/material/Card'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableContainer from '@mui/material/TableContainer'
+import TableRow from '@mui/material/TableRow'
+import TableHead from '@mui/material/TableHead'
+import PageWrapper from '@/components/theme/pages-sections/vendor-dashboard/page-wrapper'
+import Scrollbar from '@/components/theme/scrollbar'
+
+import { StyledTableHeaderCell, StyledTableRow, StyledTableCell } from '@/components/theme/pages-sections/vendor-dashboard/styles'
+
 import { auth } from '@/lib/auth'
 
 import { getSubjects } from '@/components/subject/queries'
+
+const tableHeading = [{
+    id: "code",
+    label: "Код",
+    align: "center"
+}, {
+    id: "name",
+    label: "Название",
+    align: "left"
+}];
 
 export default async function SubjectList() {
     const subjects = await getSubjects()
@@ -8,27 +29,36 @@ export default async function SubjectList() {
     if (session?.user?.role !== "admin") return <div>Not authenticated</div>
 
     return (
-        <div className="row">
-            <div className="col-md-12">
-                <div className="statement_table table-responsive">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                            <th>Код</th>
-                                <th>Субъект</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {subjects.map((subject) => (
-                                <tr key={subject.code}>
-                                    <td>{subject.code.toString().padStart(2, '0')}</td>
-                                    <td>{subject.name}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+        <PageWrapper title="Субъекты">
+            <Card>
+                <Scrollbar>
+                    <TableContainer sx={{ minWidth: 900 }}>
+                        <Table>
+                            <TableHead sx={{ backgroundColor: "grey.200" }}>
+                                <TableRow>
+                                    {tableHeading.map(headCell => (
+                                        <StyledTableHeaderCell key={headCell.id} align={headCell.align}>
+                                            {headCell.label}
+                                        </StyledTableHeaderCell>)
+                                    )}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {subjects.map(subject => (
+                                    <StyledTableRow key={subject.code} tabIndex={-1}>
+                                        <StyledTableCell align="center">
+                                            {subject.code.toString().padStart(2, '0')}
+                                        </StyledTableCell>
+                                        <StyledTableCell align="left">
+                                            {subject.name}
+                                        </StyledTableCell>
+                                    </StyledTableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Scrollbar>
+            </Card>
+        </PageWrapper>
     )
 }
