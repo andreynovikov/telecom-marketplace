a5ca606817bc74fb1c61c5448feedbf3ba80d2c9
Andrey Novikov <novikov@gmail.com>
2024-08-30 12:46:19 +0300

    Configure lazy images

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,10 +3,25 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+import pkg from '@next/env';
+const { loadEnvConfig } = pkg;
+
+loadEnvConfig(".")
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: process.env.MEDIA_PROTOCOL,
+                hostname: process.env.MEDIA_HOSTNAME,
+                port: process.env.MEDIA_PORT,
+                pathname: '/media/**',
+            },
+        ],
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')]
     }
