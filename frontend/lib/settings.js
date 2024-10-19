b3c6e277d1ea5abaddb822e5d96ca2a7764f1db6
Andrey Novikov <novikov@gmail.com>
2024-10-19 20:57:12 +0300

    E-mails

--- frontend/lib/settings.js
+++ frontend/lib/settings.js
@@ -7,3 +7,16 @@ export const titleShort = `ООО «${name}»`
 export const titleLong = `Общество с ограниченной ответственностью «${name}»`
 export const address = '192019, г. Санкт-Петербург, наб. Обводного канала, д. 27 литера Д, офис 157.2'
 export const phone = '+7 911 990-35-31'
+
+export const emails = [
+    {
+        address: 'remont@sinaps-telecom.ru',
+        title: 'Ремонт оборудования',
+        description: 'по вопросам ремонта телекоммуникацинного оборудования'
+    },
+    {
+        address: 'project@sinaps-telecom.ru',
+        title: 'СМР/ПНР, тех. поддержка, модернизация',
+        description: 'по вопросам реализации проектов СМР/ПНР, технической поддержки и модернизации телекоммуникационного оборудования'
+    }
+]
