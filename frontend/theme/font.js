a2f58fbce4f05356d9793c4c0001b5681aee129a
Andrey Novikov <novikov@gmail.com>
2024-10-19 20:40:02 +0300

    Legal data

--- frontend/theme/font.js
+++ frontend/theme/font.js
@@ -1,3 +1,6 @@
 import { Inter } from 'next/font/google'
 
-export const inter = Inter({ subsets: ['latin', 'cyrillic'] })
+export const inter = Inter({
+    subsets: ['latin', 'cyrillic'],
+    display: 'block'
+})
