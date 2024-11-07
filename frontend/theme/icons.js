1613db0d8dbcb05a45fbd8827b075c7ba0e46503
Andrey Novikov <novikov@gmail.com>
2024-11-07 15:44:50 +0300

    More icons

--- frontend/theme/icons.js
+++ frontend/theme/icons.js
@@ -1,6 +1,14 @@
 import { SvgIcon } from '@mui/material'
 
-import { IconPaperclip, IconPencil, IconTrash } from '@tabler/icons-react'
+import {
+    IconMinus,
+    IconPaperclip,
+    IconPencil,
+    IconPlus,
+    IconTrash,
+    IconEye,
+    IconX
+} from '@tabler/icons-react'
 
 export function makeSvgIcon(component, props) {
     return <SvgIcon component={component} {...props} sx={{ fill: 'none', ...props?.sx }} />
@@ -9,3 +17,7 @@ export function makeSvgIcon(component, props) {
 export const EditIcon = (props) => makeSvgIcon(IconPencil, props)
 export const DeleteIcon = (props) => makeSvgIcon(IconTrash, props)
 export const AttachIcon = (props) => makeSvgIcon(IconPaperclip, props)
+export const ViewIcon = (props) => makeSvgIcon(IconEye, props)
+export const CloseIcon = (props) => makeSvgIcon(IconX, props)
+export const PlusIcon = (props) => makeSvgIcon(IconPlus, props)
+export const MinusIcon = (props) => makeSvgIcon(IconMinus, props)
